033091649d9277f5cbd23453fcda2f9403448c29
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-02-23 00:17:27 +0800

    Working commit

--- Algorithms/Warmup/7 - Time Conversion.py
+++ Algorithms/Warmup/7 - Time Conversion.py
@@ -6,8 +6,13 @@ import sys
 timestr = input().strip()
 pm = (timestr[8:10] == "PM")
 if pm:
-	hours = int(timestr[0:2]) + 12
-	res = str(hours).zfill(2) + str(timestr[2:8])
+	hours = int(timestr[0:2])
+	if (hours != 12):
+		hours += 12
+	res = format(hours, "02") + timestr[2:8]
 	print(res)
+# then am...
+elif (timestr[0:2] == "12"):
+	print("00" + timestr[2:8] )
 else:
 	print(timestr[0:8])
