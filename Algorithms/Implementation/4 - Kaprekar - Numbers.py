712b69a3f18f61c3def396028f6dc302b0bfa2fe
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-02-27 23:57:26 +0800

    Encryption

--- Algorithms/Implementation/4 - Kaprekar - Numbers.py
+++ Algorithms/Implementation/4 - Kaprekar - Numbers.py
@@ -6,19 +6,18 @@ import math
 def digits(n):
 	return int(math.log10(n)) + 1
 
-p = 1 #int(input().strip()) #lower bound
-q = 99 #int(input().strip()) #upper bound
+p = int(input().strip()) #lower bound
+q = int(input().strip()) #upper bound
 
 count = 0
 
 for n in range(p, q + 1):
 	n2 = n**2
-	ndig = digits(n)
-	powdig = pow(10, ndig)
-	proof = (n2 % powdig) + (n2 // powdig)
-	#print (proof)
-	if (proof == n):
-		print(n)
+	powdig = pow(10, digits(n))
+	l = (n2 % powdig)
+	r = (n2 // powdig)
+	if ((l + r) == n):
+		print (n, end=" ")
 		count = count + 1
-
-print(count)
+if (count == 0):
+	print("INVALID RANGE")
