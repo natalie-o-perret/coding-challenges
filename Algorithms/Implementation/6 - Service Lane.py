5d40b9bae44109d1b1d1d439655d70ec53fe8b97
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-02-29 23:48:34 +0800

    Service Lane

---
+++ Algorithms/Implementation/6 - Service Lane.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+
+n,t = input().strip().split(' ')
+n,t = [int(n),int(t)]
+width = [int(width_temp) for width_temp in input().strip().split(' ')]
+for a0 in range(t):
+    i,j = input().strip().split(' ')
+    i,j = [int(i),int(j)]
