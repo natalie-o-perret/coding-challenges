fae3a94e8494ff5e7fc400a3ba5f29c139013b1c
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-03-15 19:04:56 +0800

    Hacker Rank: Bit Manipulation

---
+++ Algorithms/Bit Manipulation/1 - Lonely Integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/py
+
+def lonelyinteger(b):
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    a = int(input())
+    b = map(int, input().strip().split(" "))
+    print(lonelyinteger(b))
