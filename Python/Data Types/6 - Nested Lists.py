b1530af63e6ac91ac03dbc5ac6b215d0dbb80c3e
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-03-16 15:37:34 +0800

    String algorithms

--- Python/Data Types/6 - Nested Lists.py
+++ Python/Data Types/6 - Nested Lists.py
@@ -1,5 +1,6 @@
 n = int(input())
 marksheet = [(input(), float(input())) for _ in range(n)]
 marks = [mark for name, mark in marksheet]
-second_highest = sorted((set(marks))[1]
-print("\n".join(name for [name, grade] in marksheet if grade == second_highest))
+second_highest = sorted(set(marks))[1]
+names = [name for [name, grade] in marksheet if grade == second_highest]
+print("\n".join(sorted(names)))
