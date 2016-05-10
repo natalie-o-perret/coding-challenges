f3daccaff5ad786b18972dff8a90b3acb7ddcd42
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-05-10 19:46:53 +0800

    30 days of code, again

---
+++ Tutorials/30 Days of Code/Day 4 - Class vs. Instance.py
@@ -0,0 +1,21 @@
+
+class Person:
+	# Add some more code to run some checks on initialAge
+	def __init__(self, initialAge):
+	self._initialAge
+	
+	def amIOld(self):
+	# Do some computations in here and print out the correct statement to the console
+	def yearPasses(self):
+	# Increment the age of the person in here      
+
+T = int(input())
+
+for i in range(0,T):
+	age = int(input())         
+	p = Person(age)  
+	p.amIOld()
+	for j in range(0,3):
+		p.yearPasses();        
+	p.amIOld();
+	print ("")
