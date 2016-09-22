f9a5d5cae1063825989bb2f1400051cb3678a6e3
Ehouarn Perret <ehouarn.perret@outlook.com>
2016-09-22 23:01:23 +0200

    easy peasy

--- 1 - Algorithms/3 - Sorting/1 - Insertion Sort - Part 1.py
+++ 1 - Algorithms/3 - Sorting/1 - Insertion Sort - Part 1.py
@@ -1,19 +1,23 @@
 # Given a sorted list with an unsorted number e in the rightmost cell,
 # can you write some simple code to insert e into the list so that it remains sorted?
+def last_insertion_sort(sorted_array, value):
+	item = a[-1]
+	i = n - 1
+	while (item >= value) and (i > 0):
+		i -= 1
+		item = a[i]
+		if item < value:
+			a[i + 1] = value
+		else:
+			a[i + 1] = a[i]
+		print(*a, sep=" ")
+	if item >= value:
+		a[0] = value
+		print(*a, sep=" ")
 
-#n = int(input())
-a = [int(token) for token in "2 4 6 8 3".split()]
+
+n = int(input())
+a = [int(token) for token in input().split()]
 e = a.pop()
 a.append(a[-1])
-
-for i in range(len(a) - 1, 0, -1):
-	print(*a, sep=" ")
-	item = a[i]
-	if item < e:
-		a[i] = a[i + 1]
-	else:
-		a[i] = e
-
-
-
-
+last_insertion_sort(a, e)
