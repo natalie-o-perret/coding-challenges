6780bf14de8adeb483efa0dd4a6315e79626ed1f
JaiSharma287 <51850031+JaiSharma287@users.noreply.github.com>
2019-06-26 15:25:52 +0530

    Update Day 8 - Dictionaries and Maps.py
    In the question, it's given that an unknown number of names will be given to query our phone book. The code that was written here will take only N number of inputs for the query which will show an error when the number of queries exceeds N, EOF error.
    The code that I have provided will take input9 until the user stops, which is what the question demands.

--- 0 - Tutorials/30 Days of Code/Day 8 - Dictionaries and Maps.py
+++ 0 - Tutorials/30 Days of Code/Day 8 - Dictionaries and Maps.py
@@ -18,9 +18,12 @@ for _ in range(0, N):
 	name, number = input().split()
 	d[name] = number
 
-for _ in range(0, N):
-	name = input()
-	if name in d:
-		print("{}={}".format(name, d[name]))
-	else:
-		print("Not found")
+try:
+	while True:
+		a=input()
+		if a in d:
+			print("{}={}".format(a,d[a])
+		else:
+			print("Not found")
+	except EOFError:
+			      pass
