4b141dfb750c7e93479f365c35d7acc4068e402f
Saarthak Sangamnerkar <31529049+saarthaksangam@users.noreply.github.com>
2020-04-17 17:54:56 +0530

    Update Day 29 - Bitwise AND.py

--- 0 - Tutorials/30 Days of Code/Day 29 - Bitwise AND.py
+++ 0 - Tutorials/30 Days of Code/Day 29 - Bitwise AND.py
@@ -1,3 +1,16 @@
-t = int(input())
-for a0 in range(t):
-    n, k = map(int, input().split())
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    t = int(input())
+
+    for t_itr in range(t):
+        n , k = map(int , input().split())
+        print(k-1 if ((k-1) | k) <= n else k-2)
