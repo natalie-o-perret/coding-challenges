ee8230f19884a9f533fd6a0f25bdd80c0cc98c32
gajanan badge <gajubadge11@gmail.com>
2020-10-04 00:13:01 +0530

    Update 2 - No Idea.py
    improving readability

--- 6 - Python/Sets/2 - No Idea.py
+++ 6 - Python/Sets/2 - No Idea.py
@@ -1,11 +1,11 @@
 n, m = map(int, input().split())
-s = map(int, input().split())
+array = map(int, input().split())
 a = set(map(int, input().split()))
 b = set(map(int, input().split()))
 happiness = 0
-for x in s:
+for x in array:
 	if (x in a):
 		happiness += 1
-	if (x in b):
+	elif (x in b):
 		happiness -= 1
 print(happiness)
